85adf5b4ab65b4ebb8302d2cb1aeae816458a8c4
dev-kt07 <krishnatonk939@gmail.com>
2025-06-11 18:14:52 +0530

    adding functionality

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 
 provider "aws" {
   region = "us-east-2"
+}
 
-}module "vpc" {
+module "vpc" {
   source             = "./modules/vpc"
   vpc_cidr           = "10.1.0.0/16"
   public_subnets     = ["10.1.1.0/24", "10.1.2.0/24"]
